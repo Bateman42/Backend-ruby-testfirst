a5f46e6bcb89e9e99a77dd2149382e7abcf7c134
Anastasiia Bateman <Anastasiia.bateman@gmail.com>
2021-04-20 21:41:15 -0400

    work on lesson 5 completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
 #write your code here
+def translate(string)
+    vowels = %w[a e i o u]
+    words = string.split
+    new_words = []
+
+    words.each do |word|
+        new_word = 
+            if word.match /^.*qu/
+                first_part = word[/^.*qu/]
+                last_part = word.delete first_part
+                last_part + first_part + 'ay'
+            else 
+                first_part = word[/[^aeiou]*/]
+                last_part = word.delete first_part
+                last_part + first_part + 'ay'
+            end
+
+        new_word.push new_word
+    end
+
+    new_words.join ' '
+end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+
+
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0 
+  end 
+
+  def time_string 
+    second = @seconds % 60 #seconds over minutes and hours
+    minute = @seconds / 60 # can be fractional
+    hour = minute / 60
+    minute = minute % 60
+    padded(hour) + ":" + padded(minute) + ":" + padded(second)
+  end
+
+  def padded(number)
+    return_result = number >= 10? number.to_s: "0#{number}"
+  end
 end
